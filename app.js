ca847afc616eea8dd5f8cb69d484331fc1f7cb95
Sajjad Anwar <me@sajjad.in>
2015-10-15 17:12:57 +0530

    remove geocoder for reals

--- app.js
+++ app.js
@@ -12,11 +12,6 @@ L.Routing.control({
         L.latLng(57.74, 11.94),
         L.latLng(57.6792, 11.949)
     ],
-    geocoder: L.Control.Geocoder.nominatim(),
-    // NOTE: This is my (Per Liedman) demo token. Please DO NOT use
-    // it in your application. You need to get your own Mapbox account and token.
-    // I *WILL* recycle this token once in a while, and your app while break if
-    // you use this token. Thank you.
     router: new L.Routing.Mapbox('pk.eyJ1IjoiZ2VvaGFja2VyIiwiYSI6ImFIN0hENW8ifQ.GGpH9gLyEg0PZf3NPQ7Vrg'),
     routeWhileDragging: true
 }).addTo(map);
